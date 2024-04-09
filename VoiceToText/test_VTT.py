d58f2035af8b712fec8a11fa42a08f97011388b8
Dereck125 <dereck_miguel@ciencias.unam.mx>
2024-04-08 23:36:45 -0600

    Se creo la función voice to text con whisper
    La función voice to text, funcionará para obtener los textos de forma mas rápida de los audios

---
+++ VoiceToText/test_VTT.py
@@ -0,0 +1,6 @@
+import voice_to_text as vtt
+
+inpath = "C:/Users/derec/OneDrive/Escritorio/audios_test"
+outpath = "C:/Users/derec/OneDrive/Escritorio/"
+
+vtt.voice_to_text(inpath,outpath,language="spanish")
