cf2544f8f24fe9da492a05e921232aca13143d02
Dereck125 <dereck_miguel@ciencias.unam.mx>
2024-04-09 22:56:12 -0600

    Create mel_creator_test.py
    Uso de la función Mel_creator

---
+++ Mel_creator/mel_creator_test.py
@@ -0,0 +1,5 @@
+import Mel_creator as mc
+input_path = 'E:\\Github\\F-voice\\F-VOICE\\Mel_creator\\wavs2'
+output_path = 'E:\\Github\\F-voice\\F-VOICE\\Mel_creator\\wavs_mel'
+
+mc.mel_creator(input_path,output_path)
