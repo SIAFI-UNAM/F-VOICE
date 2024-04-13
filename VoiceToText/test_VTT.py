0fe42389eb22c2cbdf454c8817b135b6f7f8d970
Dereck125 <dereck_miguel@ciencias.unam.mx>
2024-04-13 13:27:03 -0600

    text

--- VoiceToText/test_VTT.py
+++ VoiceToText/test_VTT.py
@@ -1,11 +1,11 @@
 import voice_to_text as vtt
 
-#inpath = "C:/Users/derec/OneDrive/Escritorio/audios_test"
-#outpath = "C:/Users/derec/OneDrive/Escritorio/"
+inpath = "C:/Users/derec/OneDrive/Escritorio/audios_test"
+outpath = "C:/Users/derec/OneDrive/Escritorio/"
 
 
-inpath ="E:\\Github\\F-voice\\F-VOICE\\audio_test"
-outpath = "E:/Github/F-voice/F-VOICE"
+#inpath ="E:\\Github\\F-voice\\F-VOICE\\audio_test"
+#outpath = "E:/Github/F-voice/F-VOICE"
 
 vtt.voice_to_text(inpath,outpath,language="english")
 #vtt.voice_to_text(inpath,outpath,language="spanish")
