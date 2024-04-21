15687e3dc624fa98052ec57c16711a49c62544b2
Eckert11 <eckert.baezag@gmail.com>
2024-04-20 19:24:19 -0600

    Ajuste de parámetros de segmentación y verificación de duración del segmento

--- segmentationaudio.py
+++ segmentationaudio.py
@@ -9,14 +9,18 @@ def segment_and_save_audio(input_audio_path, output_folder):
     audio = audio.low_pass_filter(3000)
 
     #Segmentar el audio en tramos de 10 a 15 segundos
-    segments = split_onsilence(audio, min_silence_len=1000,silence_tresh=-40,keep_silence=100)
+    segments = split_onsilence(audio, min_silence_len=3000,silence_tresh=-50,keep_silence=100)
 
     #Crear carpeta de salida
     os.makedirs(output_folder, exist_ok=True)
 
     #Guardar los segmentos en la carpeta
     for i, segment in enumerate(segments):
-        segment.export(os.path.join(output_folder, f"segment_{i}.wav"), format="wav")
+        #verificar la duración del segmento
+        if len(segment) >= 10000 and len(segment) <= 15000: #Duración de 10 a 15 segundos
+            segment.export(os.path.join(output_folder, f"segment_{i}.wav"), format="wav")
+        else:
+            print(f"egmento {i} es demasiado corto o largo. Duración: {len(segment)} ms")
 
     return output_folder
 
