dd13a2375a2d4a6dffba37227570fe9aab091894
Dereck125 <dereck_miguel@ciencias.unam.mx>
2024-05-06 05:10:09 -0600

    Cambios a funciones para preparacion del dats set
    Se hicieron varios cambios a las funciones de preparación del data set

--- F-VOICE-1/Audios_Download/Audio_segmented_v1.py
+++ F-VOICE-1/Audios_Download/Audio_segmented_v1.py
@@ -3,12 +3,13 @@ from pydub import AudioSegment
 import os
 import numpy as np
 import noisereduce as nr
+from tqdm import tqdm
 
 # Function to segment audio
-def segment_audii(input_audio_path, output_folder, s=10):
+def segment_audio(input_audio_path, output_folder, s=10):
     # Check if s is between 10 and 15
     if s < 10 or s > 15:
-        print("Error: La duración de los segmentos debe estar entre 10 y 15 segundos.")
+        print("Error: Duration msut be between 10 to 15 seconds.")
         return False
     # Load the audio file
     audio = AudioSegment.from_file(input_audio_path)
@@ -28,14 +29,17 @@ def segment_audii(input_audio_path, output_folder, s=10):
     # Calculate the number of segments
     lon_a = len(audio)
     milliseconds_u = s * 1000
-    segments = lon_a / milliseconds_u
+    segments = int(lon_a / milliseconds_u)
     # Create the output folder
     os.makedirs(output_folder, exist_ok=True)
-    for i in range(int(segments)):
+    for i in tqdm(range(int(segments)), desc="Segmenting audio"):
         audio_i = audio[i*milliseconds_u:milliseconds_u*(i+1)]
-        # Save the segmented audios with the first characters of the audio names
-        audio_i.export(os.path.join(output_folder, f"{input_audio_path.split('/')[-1][:5]}_segment_{i}.wav"), format="wav")
+        if len(input_audio_path.split('\\')) == 2:
+            # Save the segmented audios with the first characters of the audio names
+            audio_i.export(os.path.join(output_folder, f"{input_audio_path.split('\\')[1]}_segment_{i}.wav"), format="wav")
+        else:
+            audio_i.export(os.path.join(output_folder, f"{input_audio_path.split('/')[1]}_segment_{i}.wav"), format="wav")
     # Print a success message
     print("La operación fue realizada con éxito.")
     return output_folder
-# Function to display user options
+
