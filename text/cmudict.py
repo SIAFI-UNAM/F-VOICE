8a1e87d46c2a5fa5e7c2a067e1c23a3596d3b776
Dereck125 <dereck_miguel@ciencias.unam.mx>
2024-09-19 10:15:56 -0600

    Edit files from text preprocessing
    We edited the phoneme dictionary and symbols for preprocessing

--- text/cmudict.py
+++ text/cmudict.py
@@ -1,108 +1,32 @@
 """ from https://github.com/keithito/tacotron """
 
 import re
-
-
+#Valid symbols for spanish language phonemes
 valid_symbols = [
-    "AA",
-    "AA0",
-    "AA1",
-    "AA2",
-    "AE",
-    "AE0",
-    "AE1",
-    "AE2",
-    "AH",
-    "AH0",
-    "AH1",
-    "AH2",
-    "AO",
-    "AO0",
-    "AO1",
-    "AO2",
-    "AW",
-    "AW0",
-    "AW1",
-    "AW2",
-    "AY",
-    "AY0",
-    "AY1",
-    "AY2",
-    "B",
-    "CH",
-    "D",
-    "DH",
-    "EH",
-    "EH0",
-    "EH1",
-    "EH2",
-    "ER",
-    "ER0",
-    "ER1",
-    "ER2",
-    "EY",
-    "EY0",
-    "EY1",
-    "EY2",
-    "F",
-    "G",
-    "HH",
-    "IH",
-    "IH0",
-    "IH1",
-    "IH2",
-    "IY",
-    "IY0",
-    "IY1",
-    "IY2",
-    "JH",
-    "K",
-    "L",
-    "M",
-    "N",
-    "NG",
-    "OW",
-    "OW0",
-    "OW1",
-    "OW2",
-    "OY",
-    "OY0",
-    "OY1",
-    "OY2",
-    "P",
-    "R",
-    "S",
-    "SH",
-    "T",
-    "TH",
-    "UH",
-    "UH0",
-    "UH1",
-    "UH2",
-    "UW",
-    "UW0",
-    "UW1",
-    "UW2",
-    "V",
-    "W",
-    "Y",
-    "Z",
-    "ZH",
+    "a", "e", "i", "o", "u", "b", "d̪", "c", "f",
+    "g", "k", "l", "m", "n", "ɲ", "ŋ", "p", "r",
+    "ɾ", "s", "t̪", "x", "w", "j", "z", "tʃ", "ɡ",
+    "dʒ", "ʎ", "β", "ð", "ɣ", "ç", "ɟ", "ɟʝ", "ʃ",
+    "ʝ", "θ"
 ]
 
+#Created the set of symbols
 _valid_symbol_set = set(valid_symbols)
 
 
-class CMUDict:
-    """Thin wrapper around CMUDict data. http://www.speech.cs.cmu.edu/cgi-bin/cmudict"""
+class Spanish_CMUDict:
+    """Creates a dictionary mapping from a Spanish word to its ARPAbet pronunciations."""
 
-    def __init__(self, file_or_path, keep_ambiguous=True):
+    def __init__(self, file_or_path=None, keep_ambiguous=True):
+        # Load the CMUDict dictionary from file.
         if isinstance(file_or_path, str):
-            with open(file_or_path, encoding="latin-1") as f:
+            with open(file_or_path, encoding="utf-8") as f:
+                #Convert into a python dictionary
                 entries = _parse_cmudict(f)
         else:
             entries = _parse_cmudict(file_or_path)
         if not keep_ambiguous:
+            #Remove the ambiguous pronunciations and keep only one pronunciation
             entries = {word: pron for word, pron in entries.items() if len(pron) == 1}
         self._entries = entries
 
@@ -113,26 +37,31 @@ class CMUDict:
         """Returns list of ARPAbet pronunciations of the given word."""
         return self._entries.get(word.upper())
 
-
+# Regular expression matching an apostrophe followed by a number (e.g. '10')
 _alt_re = re.compile(r"\([0-9]+\)")
 
 
 def _parse_cmudict(file):
+    """Parse the CMUDict dictionary file."""
     cmudict = {}
     for line in file:
-        if len(line) and (line[0] >= "A" and line[0] <= "Z" or line[0] == "'"):
-            parts = line.split("  ")
-            word = re.sub(_alt_re, "", parts[0])
-            pronunciation = _get_pronunciation(parts[1])
-            if pronunciation:
-                if word in cmudict:
-                    cmudict[word].append(pronunciation)
-                else:
-                    cmudict[word] = [pronunciation]
+        if len(line.strip()) == 0 or not line[0].isalpha():
+             continue
+        parts = line.strip().split('\t')
+        #First column have the word
+        word = re.sub(_alt_re, "", parts[0])
+        #Last column has the pronunciation
+        pronunciation = _get_pronunciation(parts[-1])
+        if pronunciation:
+            if word in cmudict:
+                cmudict[word].append(pronunciation)
+            else:
+                cmudict[word] = [pronunciation]
     return cmudict
 
 
 def _get_pronunciation(s):
+    """Convert ARPAbet to CMU pronunciation."""
     parts = s.strip().split(" ")
     for part in parts:
         if part not in _valid_symbol_set:
